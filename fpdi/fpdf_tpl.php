8548652af125679aa5e155a12437e6d1c7e5f769
Vasiliy Zaytsev <vasiliy.zaytsev@ffwagency.com>
2016-02-18 14:25:17 +0200

    fixed declaration error of parent class.

--- fpdi/fpdf_tpl.php
+++ fpdi/fpdf_tpl.php
@@ -272,7 +272,7 @@ class FPDF_TPL extends FPDF {
     /**
      * See FPDF/TCPDF-Documentation ;-)
      */
-    public function SetFont($family, $style = '', $size = 0) {
+    public function SetFont($family, $style = '', $size = 0, $fontfile='', $subset='default', $out=true) {
         if (is_subclass_of($this, 'TCPDF')) {
         	$args = func_get_args();
         	return call_user_func_array(array($this, 'TCPDF::SetFont'), $args);
